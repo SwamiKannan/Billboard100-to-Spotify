e764a3f3202474d60fa809f27c5045c54dfe0551
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2023-07-19 12:13:20 +0530

    Create main.py
    Tying it all up together

---
+++ main.py
@@ -0,0 +1,10 @@
+from Spotify_playlist_creator import SpotifyPlaylist
+
+
+def main():
+    playlist_spotify = SpotifyPlaylist()
+    playlist_spotify.create_final_playlist()
+
+
+if __name__ == '__main__':
+    main()
